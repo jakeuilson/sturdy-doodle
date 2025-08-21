88110d9affb0e977bdd24e0fb32f3a3ea63c2c78
Jake Uilson <jakeuilson@protonmail.com>
2025-08-21 16:17:19 +0000

    Created Models with Factory

--- sturdeer/database/seeders/DatabaseSeeder.php
+++ sturdeer/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Blog;
+use App\Models\Trend;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $trends = [
+            'Most Commented',
+            'Most Liked',
+            'Newest',
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        foreach ($trends as $trend) {
+            Trend::create(['name' => $trend]);
+        }
+
+        Blog::factory(10)->create();
+
     }
 }
